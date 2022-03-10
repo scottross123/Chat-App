4083889fe5e051012e858d74f7930aff792f4130
Scott Ross <scott.william.ross2@gmail.com>
2022-03-09 19:23:50 -0500

    actual test

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../models/database_service.dart';
+import '../services/database_service.dart';
 import 'login.dart';
 
 class RegisterPage extends StatefulWidget {
